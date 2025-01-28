4b548afb871d9498a48f51550aee4ae3503988c3
Gaganmeet Bahri <gaganmeet.bahri96@gmail.com>
2025-01-28 12:29:45 +0530

    fix manifest

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
+    registerType: 'prompt',
     injectRegister: false,
 
     pwaAssets: {
