6e68fb001c07caa3edbcda3f9a91a6c7baf6c27f
Gaganmeet Bahri <gaganmeet.bahri96@gmail.com>
2025-01-28 12:14:44 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+import { HStack } from "@chakra-ui/react"
+
+const App = () => {
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  )
+}
+export default App
